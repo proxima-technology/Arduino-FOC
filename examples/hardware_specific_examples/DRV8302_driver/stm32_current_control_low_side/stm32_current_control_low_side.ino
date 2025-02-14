a5ebc7c48dcd7f41ee9a214ecbe428c785087d0f
nrnrKinjo <kinjo.research@gmail.com>
2025-02-14 18:41:37 +0900

    update ino file

--- examples/hardware_specific_examples/DRV8302_driver/stm32_current_control_low_side/stm32_current_control_low_side.ino
+++ examples/hardware_specific_examples/DRV8302_driver/stm32_current_control_low_side/stm32_current_control_low_side.ino
@@ -11,77 +11,108 @@
  * check the https://docs.simplefoc.com for full list of motor commands
  *
  */
-#include <SimpleFOC.h>
+#include "SimpleFOC.h"
 
 // DRV8302 pins connections
 // don't forget to connect the common ground pin
-#define INH_A PA8
-#define INH_B PA9
-#define INH_C PA10
+#define INH_A 9
+#define INH_B 10
+#define INH_C 11
 
-#define EN_GATE PB7
-#define M_PWM PB4
-#define M_OC PB3
-#define OC_ADJ PB6
-#define OC_GAIN PB5
+#define EN_GATE 8
+#define M_PWM 6
+#define M_OC 5
+#define OC_ADJ 7
+// #define OC_GAIN PB5
 
-#define IOUTA PA0
-#define IOUTB PA1
-#define IOUTC PA2
+#define IOUTA A3
+#define IOUTB A4
+#define IOUTC A5
+
+#define SUPPLY_VOLTAGE A2
 
 // Motor instance
-BLDCMotor motor = BLDCMotor(7);
+BLDCMotor motor = BLDCMotor(10);
 BLDCDriver3PWM driver = BLDCDriver3PWM(INH_A, INH_B, INH_C, EN_GATE);
 
 // DRV8302 board has 0.005Ohm shunt resistors and the gain of 12.22 V/V
 LowsideCurrentSense cs = LowsideCurrentSense(0.005f, 12.22f, IOUTA, IOUTB, IOUTC);
 
 // encoder instance
-Encoder encoder = Encoder(PB14, PB15, 2048);
+Encoder encoder = Encoder(2, 3, 1024);
 
 // Interrupt routine intialisation
 // channel A and B callbacks
-void doA(){encoder.handleA();}
-void doB(){encoder.handleB();}
-
+void doA() {
+  encoder.handleA();
+}
+void doB() {
+  encoder.handleB();
+}
 
 // commander interface
 Commander command = Commander(Serial);
-void onMotor(char* cmd){ command.motor(&motor, cmd); }
+void onMotor(char* cmd) {
+  command.motor(&motor, cmd);
+}
 
 void setup() {
+  Serial.begin(921600);
 
-  // use monitoring with serial 
-  Serial.begin(115200);
-  // enable more verbose output for debugging
-  // comment out if not needed
-  SimpleFOCDebug::enable(&Serial);
+  pinMode(SUPPLY_VOLTAGE, INPUT);
 
+  //Waiting vlotage supply
+  Serial.println("Waiting!");
+  while(1){
+    delay(10);
+    if(analogRead(SUPPLY_VOLTAGE) > 300){
+      break;
+    }
+  }
+
+  //"Check communication"
+  Serial.println("Check communication");
+  while(1){
+    if(Serial.read() == 'R'){
+      break;
+    }
+    delay(10);
+  }
+
+  //Recheck the supply voltage
+  Serial.println("Recheck the supply");
+  while(1){
+    if(analogRead(SUPPLY_VOLTAGE) > 300){
+      break;
+    }
+  }
+
+  Serial.println("START calibration");
+  
   // initialize encoder sensor hardware
   encoder.init();
   encoder.enableInterrupts(doA, doB);
-  // link the motor to the sensor
+  // link the motor to the sensor multi-character character constant
   motor.linkSensor(&encoder);
 
   // DRV8302 specific code
   // M_OC  - enable overcurrent protection
-  pinMode(M_OC,OUTPUT);
-  digitalWrite(M_OC,LOW);
+  pinMode(M_OC, OUTPUT);
+  digitalWrite(M_OC, LOW);
   // M_PWM  - enable 3pwm mode
-  pinMode(M_PWM,OUTPUT);
-  digitalWrite(M_PWM,HIGH);
+  pinMode(M_PWM, OUTPUT);
+  digitalWrite(M_PWM, HIGH);
   // OD_ADJ - set the maximum overcurrent limit possible
   // Better option would be to use voltage divisor to set exact value
-  pinMode(OC_ADJ,OUTPUT);
-  digitalWrite(OC_ADJ,HIGH);
-  pinMode(OC_GAIN,OUTPUT);
-  digitalWrite(OC_GAIN,LOW);
-
+  pinMode(OC_ADJ, OUTPUT);
+  digitalWrite(OC_ADJ, HIGH);
+  // pinMode(OC_GAIN,OUTPUT);
+  // digitalWrite(OC_GAIN,LOW);
 
   // driver config
   // power supply voltage [V]
-  driver.voltage_power_supply = 19;
-  driver.pwm_frequency = 15000; // suggested under 18khz
+  driver.voltage_power_supply = 24;
+  driver.pwm_frequency = 18000;  // suggested under 18khz
   driver.init();
   // link the motor and the driver
   motor.linkDriver(&driver);
@@ -90,40 +121,40 @@ void setup() {
 
   // align voltage
   motor.voltage_sensor_align = 0.5;
-  
-  // control loop type and torque mode 
-  motor.torque_controller = TorqueControlType::voltage;
+
+  // control loop type and torque mode
+  motor.torque_controller = TorqueControlType::foc_current;
   motor.controller = MotionControlType::torque;
   motor.motion_downsample = 0.0;
-  
+
   // velocity loop PID
   motor.PID_velocity.P = 0.2;
   motor.PID_velocity.I = 5.0;
-  // Low pass filtering time constant 
+  // Low pass filtering time constant
   motor.LPF_velocity.Tf = 0.02;
   // angle loop PID
   motor.P_angle.P = 20.0;
-  // Low pass filtering time constant 
+  // Low pass filtering time constant
   motor.LPF_angle.Tf = 0.0;
-  // current q loop PID 
-  motor.PID_current_q.P = 3.0;
-  motor.PID_current_q.I = 100.0;
-  // Low pass filtering time constant 
+  // current q loop PID
+  motor.PID_current_q.P = 100.0;
+  motor.PID_current_q.I = 10;
+  // Low pass filtering time constant
   motor.LPF_current_q.Tf = 0.02;
   // current d loop PID
-  motor.PID_current_d.P = 3.0;
-  motor.PID_current_d.I = 100.0;
-  // Low pass filtering time constant 
+  motor.PID_current_d.P = 1.0;
+  motor.PID_current_d.I = 0.1;
+  // Low pass filtering time constant
   motor.LPF_current_d.Tf = 0.02;
 
-  // Limits 
-  motor.velocity_limit = 100.0; // 100 rad/s velocity limit
-  motor.voltage_limit = 12.0;   // 12 Volt limit 
-  motor.current_limit = 2.0;    // 2 Amp current limit
+  // Limits
+  motor.velocity_limit = 30.0;  // 100 rad/s velocity limit
+  motor.voltage_limit = 24.0;   // 12 Volt limit
+  motor.current_limit = 5.0;    // 2 Amp current limit
 
-  // comment out if not needed
-  motor.useMonitoring(Serial);
-  motor.monitor_variables = _MON_CURR_Q | _MON_CURR_D; // monitor the two currents d and q
+  // comment out if not needed, only use for debug
+  // motor.useMonitoring(Serial);
+  motor.monitor_variables = _MON_CURR_Q | _MON_CURR_D;  // monitor the two currents d and q
   motor.monitor_downsample = 0;
 
   // initialise motor
@@ -131,28 +162,30 @@ void setup() {
 
   cs.init();
   // driver 8302 has inverted gains on all channels
-  cs.gain_a *=-1;
-  cs.gain_b *=-1;
-  cs.gain_c *=-1;
+  cs.gain_a *= -1;
+  cs.gain_b *= -1;
+  cs.gain_c *= -1;
   motor.linkCurrentSense(&cs);
-  
+
   // align encoder and start FOC
   motor.initFOC();
 
   // set the inital target value
   motor.target = 0;
+  motor.reset_flag = false;
 
   // define the motor id
-  command.add('M', onMotor, "motor");
+  command.add('M', onMotor, "right leg motor");
 
-  Serial.println(F("Full control example: "));
-  Serial.println(F("Run user commands to configure and the motor (find the full command list in docs.simplefoc.com) \n "));
-  Serial.println(F("Initial motion control loop is voltage loop."));
-  Serial.println(F("Initial target voltage 2V."));
-
+  // Serial.println(F("Full control example: "));
+  // Serial.println(F("Run user commands to configure and the motor (find the full command list in docs.simplefoc.com) \n "));
+  // Serial.println(F("Initial motion control loop is voltage loop."));
+  // Serial.println(F("Initial target voltage 2V."));
+  
   _delay(1000);
-}
 
+  Serial.println("STANDBY");
+}
 
 void loop() {
   // iterative setting FOC phase voltage
@@ -166,4 +199,17 @@ void loop() {
 
   // user communication
   command.run();
+
+  if (motor.reset_flag) {
+    Serial.println("Reset");
+    delay(1000);
+    NVIC_SystemReset();
+  }
+
+  if(analogRead(SUPPLY_VOLTAGE) < 200){
+      Serial.println("LOW power supply");
+      Serial.println("RESET");
+      delay(1000);
+      NVIC_SystemReset();
+  }
 }
